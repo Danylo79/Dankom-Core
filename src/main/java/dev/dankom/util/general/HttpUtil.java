d18ac3a7332996e79bf5de23d7a5b80e7302413f
Danylo79 <dan.komisarenko@gmail.com>
2021-01-11 20:34:08 -0500

    Added more utils

---
+++ src/main/java/dev/dankom/util/general/HttpUtil.java
@@ -0,0 +1,39 @@
+package dev.dankom.util.general;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class HttpUtil {
+
+    private final String readUrl;
+
+    public HttpUtil(String url) {
+        this.readUrl = url;
+    }
+
+    public StringBuilder getStringBuilder() throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = getURL();
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result;
+    }
+
+    public URL getURL() {
+        try {
+            return new URL(readUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
