f5d16ab0c1d1a23f36b5b1dd5182ccc6078ff7f6
Danylo79 <dan.komisarenko@gmail.com>
2021-01-24 19:12:26 -0500

    Fixed JsonObjectBuilder

--- src/main/java/dev/dankom/file/json/JsonObjectBuilder.java
+++ src/main/java/dev/dankom/file/json/JsonObjectBuilder.java
@@ -7,18 +7,9 @@ import java.util.List;
 
 public class JsonObjectBuilder {
     private final JSONObject jsonObject;
-    private static JsonObjectBuilder instance;
 
     public JsonObjectBuilder() {
         this.jsonObject = new JSONObject();
-        JsonObjectBuilder.instance = this;
-    }
-
-    private static JsonObjectBuilder getInstance() {
-        if (instance == null) {
-            instance = new JsonObjectBuilder();
-        }
-        return instance;
     }
 
     public JsonObjectBuilder addArray(String name, List<?> contents) {
@@ -27,12 +18,12 @@ public class JsonObjectBuilder {
             array.add(o);
         }
         jsonObject.put(name, array);
-        return getInstance();
+        return this;
     }
 
     public JsonObjectBuilder addKeyValuePair(String key, Object value) {
         jsonObject.put(key, value);
-        return getInstance();
+        return this;
     }
 
     public JSONObject build() {
